a2ad5f1195099e282a8fcb59d8a6c39fecb04052
Rafael Vargas <rafael.s.vargas18@gmail.com>
2021-10-31 14:05:21 -0400

    Configuring api connection in React Front

--- ReactFront/src/index.js
+++ ReactFront/src/index.js
@@ -1,6 +1,8 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+require('dotenv').config();
 
 // Render our App
 ReactDOM.render(<App />, document.getElementById('root'));
